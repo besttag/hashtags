26bc55a9accb2e3104a99aa8d56888a823477c87
Thamerdev <100839443+besttag@users.noreply.github.com>
2022-03-09 15:34:56 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Thamer
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://topiest.blogspot.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Topiest
         </a>
       </header>
     </div>
